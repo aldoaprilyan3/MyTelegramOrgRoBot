be3cd89bd7875bbef85d2070cfe87f6b0708097c
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-03-31 07:44:57 +0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -8,14 +8,14 @@ Terima Kasih Telah Menggunakan Saya Silahkan Masukan Nomor Untuk Mendapatkan API
 /start at any stage to re-enter your details"""
     AFTER_RECVD_CODE_TEXT = """Aku Melihatnya!
 Sekarang Tolong Berikan Kode Dari telegram
-
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+Kode ini hanya digunakan untuk tujuan mendapatkan APP ID dari my.telegram.org
+Jika Anda tidak mempercayai dev bot ini, Bisa Anda Gunakan Account Tumbal.
+**[Support]https://telegram.dog/xthunderlol 
+**[ꜱʜᴀᴅᴏᴡ](t.me/coklintoud)
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@xthunderlol"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
